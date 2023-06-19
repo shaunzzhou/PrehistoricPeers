e64bc3305483a6ceaa1ae5145c517dc8e7d391d5
isabellestt <seetisabelle@gmail.com>
2023-06-19 11:23:38 +0800

    updated app.js

--- App.js
+++ App.js
@@ -4,23 +4,23 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import FontAwesome from "react-native-vector-icons/FontAwesome";
-import WelcomeScreen from "./screens/WelcomeScreen";
-import InstructionScreen from "./screens/InstructionScreen";
+import LoginSignUp from "./screens/LoginSignUp";
+import Login from "./screens/Login";
 import CustomizeScreen from "./screens/CustomizeScreen";
 import CommitmentScreenStack1 from "./screens/CommitmentScreenStack1";
 import IndividualCommitmentScreen from "./screens/IndividualCommitmentScreen";
 import GeneratingScreen from "./screens/GeneratingScreen";
 import CalendarScreen from "./screens/CalendarScreen";
-import SetCommitmentScreen from "./screens/SetCommitmentScreen";
+import SignUp from "./screens/SignUp";
 
 const Stack = createStackNavigator();
 
 function MyStack() {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Welcome" component={WelcomeScreen} />
-      <Stack.Screen name="Set Commitments" component={SetCommitmentScreen} />
-      <Stack.Screen name="Instructions" component={InstructionScreen} />
+      <Stack.Screen name="Welcome" component={LoginSignUp} />
+      <Stack.Screen name="Set Commitments" component={SignUp} />
+      <Stack.Screen name="Instructions" component={Login} />
       <Stack.Screen name="Customize" component={CustomizeScreen} />
       <Stack.Screen name="Get Commitments" component={CommitmentScreenStack1} />
       <Stack.Screen
