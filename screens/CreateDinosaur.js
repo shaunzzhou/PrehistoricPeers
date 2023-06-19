53432734c2fd1a4ab5ac740c289ba37b8cfe8e7c
chingupingu <p.shuen01@gmail.com>
2023-06-19 12:44:21 +0800

    Create CreateDinosaur.js

---
+++ screens/CreateDinosaur.js
@@ -0,0 +1,14 @@
+import * as React from "react";
+import { ImageBackground, View, StyleSheet, Image } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
+
+export default function CreateDinosaur({ navigation }) {
+  return (
+    <View>
+        <text>test </text>
+    </View>
+  );
+}
