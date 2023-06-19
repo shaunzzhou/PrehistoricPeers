5b3b3af6f81f58a77d5ce3d77792af1f150aa9ca
chingupingu <p.shuen01@gmail.com>
2023-06-19 12:11:49 +0800

    Update LoginSignUp.js

--- screens/LoginSignUp.js
+++ screens/LoginSignUp.js
@@ -1,64 +1,82 @@
 import * as React from "react";
-import { View, Text, StyleSheet, Image } from "react-native";
+import { ImageBackground, View, StyleSheet, Image } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import FontAwesome from "react-native-vector-icons/FontAwesome";
 
-export default function WelcomeScreen({ navigation }) {
+export default function LoginSignUp({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Image style={styles.logo} source={require("../assets/LOGOINPNG.png")} />
-      <Text style={styles.text}>plaNS</Text>
+      <ImageBackground 
+      source={require("../images/Login:SignUp/bg.png")}
+      style={styles.backgroundImage}>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center"}}>
       <TouchableOpacity
         style={styles.button}
-        onPress={() => navigation.navigate("Instructions")}
+        onPress={() => navigation.navigate("Login")}
       >
-        <Text style={styles.button_text}>Start Planning</Text>
+        <Image source={require("../images/Login:SignUp/login.png")} style={styles.image}/>
       </TouchableOpacity>
       <TouchableOpacity
         style={styles.button}
-        onPress={() => navigation.navigate("Set Commitments")}
+        onPress={() => navigation.navigate("SignUp")}
       >
-        <Text style={styles.button_text}>Set Commitments</Text>
+        <Image source={require("../images/Login:SignUp/signup.png")} style={styles.image}/>
       </TouchableOpacity>
     </View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
+  backgroundImage: {
+    flex: 1,
+    resizeMode: 'cover',
+  },
   button: {
-    padding: 10,
-    marginTop: 20,
-    width: 200,
-    backgroundColor: "#648839",
-    borderRadius: 10,
-    shadowColor: "#171717",
-    shadowOffset: { width: -2, height: 4 },
-    shadowOpacity: 0.15,
-    shadowRadius: 3,
-    borderWidth: 2,
-    borderColor: "`rgba(0,0,0,0.4)`",
-    justifyContent: "center",
-    alignSelf: "center",
-    marginBottom: 30,
+    width: 180,
+    height: 100,
+    top: 250,
     alignItems: "center",
-    marginBottom: 30,
+    
   },
-  button_text: {
-    fontSize: 16,
-    lineHeight: 21,
-    letterSpacing: 0.25,
-    color: "white",
+  image: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "contain",
   },
-  text: {
-    fontWeight: "bold",
-    fontSize: 60,
-    marginBottom: 30,
-  },
-  logo: {
-    width: 305.62,
-    height: 370,
-    marginBottom: 25,
-  },
+  // button: {
+  //   padding: 10,
+  //   marginTop: 20,
+  //   width: 200,
+  //   backgroundColor: "#648839",
+  //   borderRadius: 10,
+  //   shadowColor: "#171717",
+  //   shadowOffset: { width: -2, height: 4 },
+  //   shadowOpacity: 0.15,
+  //   shadowRadius: 3,
+  //   borderWidth: 2,
+  //   borderColor: "`rgba(0,0,0,0.4)`",
+  //   justifyContent: "center",
+  //   alignSelf: "center",
+  //   marginBottom: 30,
+  //   alignItems: "center",
+  //   marginBottom: 30,
+  // },
+  // button_text: {
+  //   fontSize: 16,
+  //   lineHeight: 21,
+  //   letterSpacing: 0.25,
+  //   color: "white",
+  // },
+  // text: {
+  //   fontWeight: "bold",
+  //   fontSize: 60,
+  //   marginBottom: 30,
+  // },
+  // logo: {
+  //   width: 305.62,
+  //   height: 370,
+  //   marginBottom: 25,
+  // },
 });
